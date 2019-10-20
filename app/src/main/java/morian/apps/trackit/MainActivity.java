e19d83008f37dcd4a3cf13c6948939651302f100
wmorian <wmorian@pm.me>
2019-10-20 09:09:23 +0200

    add fragments and call them on click of the nav items

--- app/src/main/java/morian/apps/trackit/MainActivity.java
+++ app/src/main/java/morian/apps/trackit/MainActivity.java
@@ -1,5 +1,6 @@
 package morian.apps.trackit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -7,8 +8,11 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
 
@@ -21,11 +25,20 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_draw_open, R.string.navigation_draw_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        // init with this fragment when first call
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new SportFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_sport);
+        }
     }
 
     @Override
@@ -35,5 +48,27 @@ public class MainActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case (R.id.nav_sport):
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new SportFragment()).commit();
+                break;
+            case (R.id.nav_work):
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new WorkFragment()).commit();
+                break;
+            case (R.id.nav_weather):
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new WeatherFragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
