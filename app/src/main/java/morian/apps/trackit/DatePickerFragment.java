fed4f18053976823eb88de156b0e6af8970b3383
wmorian <wmorian@pm.me>
2019-10-20 11:18:36 +0200

    add UI for sport fragment

---
+++ app/src/main/java/morian/apps/trackit/DatePickerFragment.java
@@ -0,0 +1,49 @@
+package morian.apps.trackit;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+    final Calendar cal = Calendar.getInstance();
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
+
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        cal.set(Calendar.YEAR, year);
+        cal.set(Calendar.MONTH, month);
+        cal.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        String selectedDate = new SimpleDateFormat("dd.MM.yyyy", Locale.ENGLISH)
+                .format(cal.getTime());
+
+        getTargetFragment().onActivityResult(
+                getTargetRequestCode(),
+                Activity.RESULT_OK,
+                new Intent().putExtra("selectedDate", selectedDate)
+        );
+    }
+}
--- app/src/main/java/morian/apps/trackit/SportFragment.java
+++ app/src/main/java/morian/apps/trackit/SportFragment.java
@@ -1,19 +1,85 @@
 package morian.apps.trackit;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class SportFragment extends Fragment {
+
+    public static final int REQUEST_CODE = 11;
+    TextView currentDate;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_sport, container, false);
+        View view = inflater.inflate(R.layout.fragment_sport, container, false);
+
+        Button datePicker = view.findViewById(R.id.date_picker);
+        currentDate = view.findViewById(R.id.current_date);
+        final FragmentManager fm = getActivity().getSupportFragmentManager();
+
+        datePicker.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                DialogFragment fragment = new DatePickerFragment();
+                fragment.setTargetFragment(SportFragment.this, REQUEST_CODE);
+                fragment.show(fm, "datePicker");
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
+        Date date = new Date();
+        currentDate.setText(formatter.format(date));
+
+        initTimeOfDaySpinner(view);
+        initKindOfSportsSpinner(view);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            String date = data.getStringExtra("selectedDate");
+            currentDate.setText(date);
+        }
+    }
+
+    private void initTimeOfDaySpinner(@NonNull View view) {
+        Spinner timeOfDay = view.findViewById(R.id.time_of_day);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.time_of_day, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        timeOfDay.setAdapter(adapter);
+
+    }
+
+    private void initKindOfSportsSpinner(@NonNull View view) {
+        Spinner kindsOfSports = view.findViewById(R.id.kinds_of_sports);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.sports, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        kindsOfSports.setAdapter(adapter);
+
     }
 }
